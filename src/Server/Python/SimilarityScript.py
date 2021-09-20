0fbb22dd7d32b68e6a545b3462a1e5755d49eda7
GangLii <gangli0825@gmail.com>
2021-09-20 15:44:06 -0500

    Update SimilarityScript.py

--- src/Server/Python/SimilarityScript.py
+++ src/Server/Python/SimilarityScript.py
@@ -1,15 +1,18 @@
 import sys
+import json
 fileName = sys.argv[1]
 
 #export interface SimilarityResult {
 #    inputFileName: string,
 #    outputFileNames: Array<[string, number]>
 #}
+out = [{'inputFileName': fileName, 'outputFileNames': [['patient00001/study1/view1_frontal.jpg', 0.3], 
+['patient00003/study1/view1_frontal.jpg', 0.4]], }]
 
-testOut = "{inputFileName: '1980138012.png', outputFileNames: [['190329.png', 0.3], ['819023.png', 0.4], ['934.png', 0.3]]}"
+# testOut = "{inputFileName: '1980138012.png', outputFileNames: [['190329.png', 0.3], ['819023.png', 0.4], ['934.png', 0.3]]}"
 ##fileName.lower() + "_" + fileName.upper()
 
 
 
-print(testOut)
+print(json.dumps(out))
 sys.stdout.flush()
