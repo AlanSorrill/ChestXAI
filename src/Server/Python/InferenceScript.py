27a13d3193e5d4bebb2b39f90092c2f1473933a6
GangLii <gangli0825@gmail.com>
2021-10-10 19:25:40 -0500

    first version

--- src/Server/Python/InferenceScript.py
+++ src/Server/Python/InferenceScript.py
@@ -1,15 +1,67 @@
-import sys
-import json
-fileName = sys.argv[1]
+'''before run the script, there are some other things to do first
+1. Download the chexpert train images to 'patients' folder, or the script can't retrieve images
+2. Make sure you have required packaged installed
+    To do this, try following commands if you have Anaconda installed
+    1) pip install libauc
+    2) pip3 install torch torchvision 
+    3) GPU+CPU version:'conda install -c pytorch faiss-gpu' 
+        or ONLY CPU version: 'conda install -c pytorch faiss-cpu
+3. Check the path variables are correct
+'''
 
-##fileName.lower() + "_" + fileName.upper()
+####*** There are some important changes and notes*** ####
+'''
+1. The script implemented realizes two functions, both predicting and similarity searching, so the script
+will print twice, the first one is predicting result, and the second is similarity result.
+2. Since we forgot to display labels of similar images, the script adds it, it's a string that have five bits indicating
+five diseases. The example can be found in later code.
+3. It's worth mentioning that the script initiates model and loads vectors could cost much time. So in the later version,
+we could consider that after initiating model and loading vector, keep the script running. Then the script can
+do next prediction and similarity search directly, without initiating the model again.
+'''
 
 #export interface InferenceResponse {
 #    inputFileName: string,
 #    diagnosis: Array<[Disease, number]>
 #}
-outInference = {'fileName': fileName, 'diagnosis': [[0, 0.85], [1, 0.25], [2, 0.99]], }
-# testOut = "{fileName: '8310923871.png', diagnosis: [[0, 0.85], [1, 0.25], [2, 0.99]]}"
 
-print(json.dumps(outInference))
-sys.stdout.flush()
+#export interface SimilarityResult {
+#    inputFileName: string,
+#    outputFileNames: Array<[string, number, string]>
+#}
+
+# Five kinds of disease:
+# Cardiomegaly(C0)
+# Edema(C1)
+# Consolidation(C2)
+# Atelectasis(C3)
+# Pleural Effusion(C4)
+
+import sys
+import json
+from SimilarityClass import SimilaritySearch
+
+
+image_base_path = '../../../public/patients/' ###To be checked
+train_csv_path = '../../../public/patients/train.csv'
+model_vectors_path = '../../../models/'
+
+
+if __name__ == '__main__':
+
+    fileName = sys.argv[1]
+
+    obj = SimilaritySearch(train_csv_path, image_base_path, model_vectors_path, cuda_or_cpu = 'cpu')
+
+    out_prediction, out_images_and_similarities = obj.run(fileName)
+    outPrediction = {'fileName': fileName, 'diagnosis': out_prediction }
+    # outPrediction = "{fileName: '8310923871.png', diagnosis: [[0, 0.85], [1, 0.25], [2, 0.99]]}"
+    outSimilarity = {'inputFileName': fileName, 'outputFileNames': out_images_and_similarities}
+
+    # outSimilarity = "{inputFileName: '1980138012.png', outputFileNames: [['190329.png', 0.3,'00010'], 
+    # ['819023.png', 0.4, '00000'], ['934.png', 0.3, '10000']]}"
+
+
+    print(json.dumps(outPrediction))
+    print(json.dumps(outSimilarity))
+    sys.stdout.flush()
---
+++ src/Server/Python/SimilarityClass.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import numpy as np
+import faiss 
+import cv2
+from libauc.models import DenseNet121
+
+import torch 
+from PIL import Image
+import torch.nn.functional as F
+
+class SimilaritySearch(object):
+    def __init__(self, train_csv_path , image_base_path, model_vectors_path, cuda_or_cpu = 'cpu',topK=10, 
+                image_size=320, use_frontal=True,
+                selected_cols=['Cardiomegaly', 'Edema', 'Consolidation', 'Atelectasis',  'Pleural Effusion'],
+                ):
+        
+        ###load data
+        self.df = pd.read_csv(train_csv_path)
+        self.df['Path'] = self.df['Path'].str.replace('CheXpert-v1.0-small/', '')
+        self.df['Path'] = self.df['Path'].str.replace('CheXpert-v1.0/', '')
+        self.df['Path'] = self.df['Path'].str.replace('train/', '')  ####to be change
+        if use_frontal:
+            self.df = self.df[self.df['Frontal/Lateral'] == 'Frontal']       
+            
+        self.images_list =  [image_base_path+path for path in self.df['Path'].tolist()]### 
+
+        self.train_vecs = np.load(model_vectors_path + 'train_vecs_pre_none.npz')['vectors'].astype('float32')
+        self.train_truth = np.load(model_vectors_path + 'train_truth_pre_none.npy').astype('int')
+        
+        index = faiss.index_factory(1024, "Flat")
+        if cuda_or_cpu == 'cuda':
+            res = faiss.StandardGpuResources()
+            self.index = faiss.index_cpu_to_gpu(res, 0, index)
+        else:
+            self.index = index
+        self.index.train(self.train_vecs)
+        self.index.add(self.train_vecs)
+        
+        self.topK = topK
+            
+        # proccess missing values 
+        for col in selected_cols:
+            if col in ['Edema', 'Atelectasis']:
+                self.df[col].replace(-1, 1, inplace=True)  
+                self.df[col].fillna(0, inplace=True) 
+            elif col in ['Cardiomegaly','Consolidation',  'Pleural Effusion']:
+                self.df[col].replace(-1, 0, inplace=True) 
+                self.df[col].fillna(0, inplace=True)
+            else:
+                self.df[col].fillna(0, inplace=True)
+                
+        self.image_size = image_size
+        
+        ###load model
+        self.device = torch.device(cuda_or_cpu)
+        self.model = DenseNet121(pretrained=False, last_activation='sigmoid', activations='relu', num_classes=5)
+        self.model.to(self.device)
+        state_dict = torch.load(model_vectors_path + 'second_stage_none.pth')#map_location={'cuda:0':'cuda:3'}
+        self.model.load_state_dict(state_dict, strict=True)
+        self.model.eval()
+        
+                
+     
+    def preprocess_image(self, image_path):
+        image = cv2.imread(image_path, 0)
+        image = Image.fromarray(image)
+
+        image = np.array(image)
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+        
+        # resize and normalize; e.g., ToTensor()
+        image = cv2.resize(image, dsize=(self.image_size, self.image_size), interpolation=cv2.INTER_LINEAR)  
+        image = image/255.0
+        __mean__ = np.array([[[0.485, 0.456, 0.406]]])
+        __std__ =  np.array([[[0.229, 0.224, 0.225]  ]]) 
+        image = (image-__mean__)/__std__
+        image = image.transpose((2, 0, 1)).astype(np.float32)
+        image = torch.from_numpy(image).unsqueeze(0)
+        #image = torch.from_numpy(image).float().permute(2, 0, 1).unsqueeze(0)
+
+        return image
+
+    def postprocess(self, y_pred, Distance, S_index):
+        
+        ###prediction
+        out_prediction = []
+        for disease, psb in enumerate(y_pred):
+            out_prediction.append([disease, psb])
+        ###images and similarities   
+        out_images_and_similarities = []
+        for i, idx in enumerate(S_index):
+            path = self.images_list[idx]
+            similarity = 1 /(1 + np.sqrt(Distance[i]))
+            label = ''.join(str(l) for l in self.train_truth[idx])
+            out_images_and_similarities.append([path, similarity,label])
+        
+        return out_prediction, out_images_and_similarities
+        
+
+    def predict_and_search(self, image):
+        image = image.to(self.device)        
+        with torch.no_grad():
+            features = self.model.features(image)
+            out = F.relu(features, inplace=True) #F.relu(features, inplace=True)
+            out = F.adaptive_avg_pool2d(out, (1, 1))
+            vector = torch.flatten(out, 1)
+            out = self.model.classifier(vector)
+            out = self.model.sigmoid(out)
+            
+            
+            query_vector = vector.detach().cpu().numpy()
+            y_pred = out.detach().cpu().numpy()
+                
+                
+        Distance, S_index = self.index.search(query_vector, self.topK)
+        out_prediction, out_images_and_simiarities = self.postprocess(y_pred[0], Distance[0], S_index[0])
+
+        return out_prediction, out_images_and_simiarities
+    
+    def run(self, image_path):
+        
+        image = self.preprocess_image(image_path)
+        out_prediction, out_images_and_similarities = self.predict_and_search(image)
+        
+                
+        return out_prediction, out_images_and_similarities
